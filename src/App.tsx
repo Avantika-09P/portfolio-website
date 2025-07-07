c04054af56e02e63615854316b2806082800111b
Avantika Padhi <avantikapadhi4822@gmail.com>
2025-07-07 21:38:43 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -137,7 +137,7 @@ function App() {
             <div className="mb-8 relative">
               <div className="profile-container">
                 <img 
-                  src="avantika.jpeg"
+                  src="/avantika.jpeg"
                   alt="Avantika Padhi" 
                   className="profile-image"
                 />
